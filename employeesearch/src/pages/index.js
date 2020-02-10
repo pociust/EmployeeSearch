da2516af92bdfc990550ab35d5c9190b19a7e9dd
Tom Pocius <poci14@gmail.com>
2020-02-09 19:37:44 -0600

    filter function to look for names

--- employeesearch/src/pages/index.js
+++ employeesearch/src/pages/index.js
@@ -5,14 +5,42 @@ import Form from './../components/Form';
 const EmployeeList = ({ initialList }) => {
   const [employees, setEmployees] = useState(initialList || []);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log('tom');
+  // filteredEmployees = (poetFilter) => {
+  //   let filteredPoets = employees
+  //   filteredPoets = filteredPoets.filter((poet) => {
+  //     let poetName = poet.nae
+  //     return poetName.indexOf(
+  //       poetFilter.toLowerCase()) !== -1
+  //   })
+  //   this.setState({
+  //     filteredPoets
+  //   })
+  // }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // let mike = event.target.employeeName.value;
+    // var i;
+    // console.log('1', event);
+    // for (i = 0; i < employees.length; i++) {
+    //   let tom = employees[i].name;
+    //   console.log('2', mike, tom);
+    // }
+
+    let employeeSearched = event.target.employeeName.value;
+    let filteredEmployees = employees.name;
+    filteredEmployees = employees.filter(
+      employee => employee.name.indexOf(employeeSearched) !== -1
+    );
+    setEmployees(filteredEmployees);
+    // console.log('1', searchedEmployee);
+
+    console.log('tom', filteredEmployees);
   };
 
-  const searchEmployee = () => {
-    console.log("checkemployee")
-  }
+  const searchEmployee = event => {
+    console.log(event.target.value);
+  };
 
   return (
     <div>

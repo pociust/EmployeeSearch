4cd4d25a64932ba80f6575229d1972d705fe7e8b
Tom Pocius <poci14@gmail.com>
2020-02-10 22:32:06 -0600

    added age input

--- employeesearch/src/components/Form.js
+++ employeesearch/src/components/Form.js
@@ -4,9 +4,9 @@ import React from 'react';
 function Form (props) {
   return (
     <form onSubmit={props.onSubmit}>
-      <input type="text" placeholder="Name" name="employeeName" onChange={props.onChange}>
-        
+      <input type="text" placeholder="Name" name="employeeName" onChange={props.onChangeName}>
       </input>
+      <input type="number" placeholder="Age" name="employeeAge" onChange={props.searchAge}></input>
     </form>
   )
 
--- employeesearch/src/pages/index.js
+++ employeesearch/src/pages/index.js
@@ -6,25 +6,28 @@ const EmployeeList = ({ initialList }) => {
   const [employees, setEmployees] = useState(initialList || []);
   const [searchedQuery, setQuery] = useState('');
   const filteredEmployees = employees.filter(
-    employee => employee.name.toLowerCase().indexOf(searchedQuery.toLowerCase()) !== -1
+    employee =>
+      employee.name.toLowerCase().indexOf(searchedQuery.toLowerCase()) !== -1
   );
 
   const handleSubmit = event => {
     event.preventDefault();
 
     setQuery(event.target.employeeName.value);
-
   };
 
   const searchEmployee = event => {
+    setQuery(event.target.value);
+  };
 
-    setQuery(event.target.value)
+  const searchAge = () => {
+    console.log('tom')
   };
 
   return (
     <div>
       <div className="frow">
-        <Form onSubmit={handleSubmit} onChange={searchEmployee}></Form>
+        <Form onSubmit={handleSubmit} onChangeName={searchEmployee} onChangeAge={searchAge}></Form>
       </div>
       {filteredEmployees.map(employee => (
         <Card

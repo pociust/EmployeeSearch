3341015925c488b2b87f91ab98ca58c52e591c78
Tom Pocius <poci14@gmail.com>
2020-02-10 21:01:13 -0600

    fixed search employee on submit, added search on key

--- employeesearch/src/pages/index.js
+++ employeesearch/src/pages/index.js
@@ -4,54 +4,34 @@ import Form from './../components/Form';
 
 const EmployeeList = ({ initialList }) => {
   const [employees, setEmployees] = useState(initialList || []);
-
-  // filteredEmployees = (poetFilter) => {
-  //   let filteredPoets = employees
-  //   filteredPoets = filteredPoets.filter((poet) => {
-  //     let poetName = poet.nae
-  //     return poetName.indexOf(
-  //       poetFilter.toLowerCase()) !== -1
-  //   })
-  //   this.setState({
-  //     filteredPoets
-  //   })
-  // }
+  const [searchedQuery, setQuery] = useState('');
+  const filteredEmployees = employees.filter(
+    employee => employee.name.indexOf(searchedQuery) !== -1
+  );
 
   const handleSubmit = event => {
     event.preventDefault();
-    // let mike = event.target.employeeName.value;
-    // var i;
-    // console.log('1', event);
-    // for (i = 0; i < employees.length; i++) {
-    //   let tom = employees[i].name;
-    //   console.log('2', mike, tom);
-    // }
 
-    let employeeSearched = event.target.employeeName.value;
-    let filteredEmployees = employees.name;
-    filteredEmployees = employees.filter(
-      employee => employee.name.indexOf(employeeSearched) !== -1
-    );
-    setEmployees(filteredEmployees);
-    // console.log('1', searchedEmployee);
+    setQuery(event.target.employeeName.value);
 
-    console.log('tom', filteredEmployees);
   };
 
   const searchEmployee = event => {
-    console.log(event.target.value);
+
+    setQuery(event.target.value)
   };
 
   return (
     <div>
       <Form onSubmit={handleSubmit} onChange={searchEmployee}></Form>
-      {employees.map(employee => (
+      {filteredEmployees.map(employee => (
         <Card
           key={employee.id}
           name={employee.name}
           occupation={employee.occupation}
         />
       ))}
+      <Card></Card>
     </div>
   );
 };
